22ddd355bdf497950ee7515694616bbf39bd88e8
Blackjack2020 <69491789+devonp702@users.noreply.github.com>
2020-11-17 22:50:55 -0700

    Works locally

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.put("/api/workouts/:id", (req, res) => {
 });
 
 app.post("/api/workouts", (req, res) => {
-  db.Workout.create(req.body)
+  db.Workout.create(req.body.data)
     .then(dbworkout => {
       res.json(dbworkout);
     })
