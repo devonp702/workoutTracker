a538cf954c9f0b6206e64e1f5c80153f1d4869ea
Blackjack2020 <69491789+devonp702@users.noreply.github.com>
2020-11-17 22:32:23 -0700

    Fixed models

--- models/Workout.js
+++ models/Workout.js
@@ -1,18 +1,34 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
   day: {
     type: Date,
-    unique: true
+    default: Date.now
   },
-  exercises: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Exercise"
+  exercises: [{
+    type: {
+      type: String
+    },
+    name: {
+      type: String
+    },
+    duration: {
+      type: Number
+    },
+    weight: {
+      type: Number
+    },
+    reps: {
+      type: Number
+    },
+    sets: {
+      type: Number
+    },
+    distance: {
+      type: Number
     }
-  ]
+  }]
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
